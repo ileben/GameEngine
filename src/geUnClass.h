b459ed04f74bc604186c6d6e1499c2e05227e25f
Ivan Leben <ivan@wonderland.(none)>
2008-09-13 04:43:27 +1000

    geUnClass.h, Makefile.am

---
+++ src/geUnClass.h
@@ -0,0 +1,26 @@
+/*
+-------------------------------------------------------------
+Undefine previous definitions to allow for a different DLL
+specification (export / import) to affect these macros
+-------------------------------------------------------------*/
+#ifdef __DECLARE
+#  undef __DECLARE
+#endif
+
+#ifdef DECLARE_CLASS
+#  undef DECLARE_CLASS
+#endif
+
+#ifdef DECLARE_SUBCLASS
+#  undef DECLARE_SUBCLASS
+#endif
+
+#ifdef DECLARE_ABSTRACT
+#  undef DECLARE_ABSTRACT
+#endif
+
+#ifdef DECLARE_SUBABSTRACT
+#  undef DECLARE_SUBABSTRACT
+#endif
+
+#undef CLASS_DLL_ACTION
