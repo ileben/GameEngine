a1188a5c74251812b69143f0720afc03f7ec3e78
unknown <Uporabnik@.(none)>
2009-01-07 15:00:44 +0100

    Moved all Actor subclasses into actors directory. Added AxisActor class.

---
+++ src/engine/actors/geAxisActor.cpp
@@ -0,0 +1,24 @@
+#define GE_API_EXPORT
+#include "geEngine.h"
+#include "geGLHeaders.h"
+
+namespace GE
+{
+  void AxisActor::render (MaterialID materialID)
+  {
+    glBegin( GL_LINES );
+    glColor3f ( 1, 0, 0 );
+    glVertex3f( 0, 0, 0 );
+    glVertex3f( 1, 0, 0 );
+
+    glColor3f ( 0, 1, 0 );
+    glVertex3f( 0, 0, 0 );
+    glVertex3f( 0, 1, 0 );
+
+    glColor3f ( 0, 0, 1 );
+    glVertex3f( 0, 0, 0 );
+    glVertex3f( 0, 0, 1 );
+    glEnd();
+  }
+
+}//namespace GE
---
+++ src/engine/actors/geAxisActor.h
@@ -0,0 +1,12 @@
+#ifndef __GEAXISACTOR_H
+#define __GEAXISACTOR_H
+
+namespace GE
+{
+  class AxisActor : public Actor
+  {
+    virtual void render (MaterialID materialID);
+  };
+}
+
+#endif//__GEAXISACTOR_H
